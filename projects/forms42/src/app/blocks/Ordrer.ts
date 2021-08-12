cebe5827e427574ca79c6da66245bee43f69eb73
Alex Michael Høffner <miamh@bladkompagniet.dk>
2021-08-12 14:14:29 +0200

    .

--- projects/forms42/src/app/blocks/Ordrer.ts
+++ projects/forms42/src/app/blocks/Ordrer.ts
@@ -1,7 +1,7 @@
 import { alias, Block, table, column, key, Column, trigger, Trigger, SQLTriggerEvent } from "forms42";
 
 @alias("ordrer")
-@table({name: "bk_ordrer", order: "id"})
+@table({name: "bk_ordrer", order: "id", where: "bkku_id is not null"})
 
 @column({name: "id"                , type: Column.integer   , mandatory: true})
 @column({name: "bkku_id"           , type: Column.integer   , mandatory: true})
