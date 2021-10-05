2643cd02892ff27c64754d302c7b8f2e916c2e47
alhof <alhof@miracle42.dk>
2021-10-05 16:17:50 +0200

    .

---
+++ projects/forms42/src/app/blocks/Addresses.ts
@@ -0,0 +1,24 @@
+import { alias, Block, table, column, key, Column } from "forms42";
+
+@alias("addresses")
+@table({name: "ks.order_addresses", order: "id"})
+
+@column({name: "id"            , type: Column.integer })
+@column({name: "husn_id"       , type: Column.integer })
+@column({name: "street_name"   , type: Column.varchar })
+@column({name: "house_number"  , type: Column.integer })
+@column({name: "house_letter"  , type: Column.varchar })
+@column({name: "floor"         , type: Column.varchar })
+@column({name: "apartment"     , type: Column.varchar })
+@column({name: "zip_code"      , type: Column.varchar })
+@column({name: "city"          , type: Column.varchar })
+@column({name: "oprettet_dato" , type: Column.datetime})
+@column({name: "oprettet_af"   , type: Column.varchar })
+@column({name: "rettet_dato"   , type: Column.datetime})
+@column({name: "rettet_af"     , type: Column.varchar })
+
+
+
+export class Addresses extends Block
+{
+}
--- projects/forms42/src/app/forms/KundeService.ts
+++ projects/forms42/src/app/forms/KundeService.ts
@@ -1,90 +1,27 @@
 import { Component } from '@angular/core';
-import { Kunder } from '../blocks/Kunder';
-import { Ordrer } from '../blocks/Ordrer';
-import { Form, Block, block, field, FieldType, trigger, Trigger, keytrigger, KeyTriggerEvent, FieldTriggerEvent, SQLTriggerEvent, Condition, Column, DateUtils, keymap, join, show, connect } from 'forms42';
+import { Addresses } from '../blocks/Addresses';
+import { Form, block, trigger, Trigger, SQLTriggerEvent, Condition } from 'forms42';
 
 
 @Component({
-    templateUrl: "kundeservice.html",
-    styleUrls: ['kundeservice.css']
+    templateUrl: "KundeService.html",
+    styleUrls: ['KundeService.css']
 })
-
-
-@field({name: "ctrl.type",      type: FieldType.text })
-@field({name: "ctrl.fra_dato",  type: FieldType.date })
-@field({name: "ctrl.til_dato",  type: FieldType.date })
-
-@join({master: {alias: "kunder", key: "primary"}, detail: {alias: "ordrer", key: "kunde"}})
-
 
 export class KundeService extends Form
 {
-    @block({component: Kunder})
-    private kunder:Kunder = null;
-
-    @block({component: Ordrer})
-    private ordrer:Ordrer = null;
+    @block({component: Addresses})
+    private addresses:Addresses = null;
 
 
-    @keytrigger(keymap.clearblock)
-    public async clrctrl(event:KeyTriggerEvent) : Promise<boolean>
-    {
-        if (event.block == 'ctrl') return(false);
-        return(true);
-    }
-
-
-    @keytrigger(keymap.clearform)
-    public async clrform(event:KeyTriggerEvent) : Promise<boolean>
-    {
-        await this.clear();
-        this.kunder.enterquery();
-        return(false);
-    }
-
-
-
-    @trigger(Trigger.PostChange,"ctrl")
-    public async ctrlChange(event:FieldTriggerEvent) : Promise<boolean>
-    {
-        if (this.connected && !this.kunder.querymode)
-            this.ordrer.executequery();
-
-        return(true);
-    }
-
-
-    @keytrigger(keymap.executequery)
-    public async alwaysQuery(event:KeyTriggerEvent) : Promise<boolean>
-    {
-        if (event.block == "kunder")
-        {
-            await this.kunder.executequery();
-            if (this.kunder.empty()) this.kunder.enterquery(true);
-            return(false);
-        }
-
-        if (event.block == "ordrer")
-        {
-            this.kunder.sendKey(keymap.executequery);
-            return(false);
-        }
-
-        return(true);
-    }
-
-
-    @trigger(Trigger.PreQuery,"kunder")
+    @trigger(Trigger.PreQuery,"addresses")
     public async prequeryKunder(event:SQLTriggerEvent) : Promise<boolean>
     {
         let ok:boolean = false;
 
-        this.kunder.searchfilter.forEach((filter) =>
+        this.addresses.searchfilter.forEach((filter) =>
         {
-            if (filter.name == "navn") ok = true;
-            if (filter.name == "adresse") ok = true;
-            if (filter.name == "abon_nr") ok = true;
-            if (filter.name == "konto_nr") ok = true;
+            if (filter.name == "street_name") ok = true;
         });
 
         if (!ok)
@@ -93,82 +30,14 @@ export class KundeService extends Form
             return(false);
         }
 
-        // Adresse is not a database field
-        // and not set as condition
-        this.kunder.searchfilter.forEach((filter) =>
-        {
-            if (filter.name == 'adresse')
-                event.stmt.whand(filter.name,'"'+filter.value+'"');
-        });
-
         let conditions:Condition[] = event.stmt.getCondition()?.split();
 
         if (conditions) conditions.forEach((cond) =>
         {
-            if (cond.column == 'navn')
-                cond.setCondition("to_tsvector('danish',navn) @@ websearch_to_tsquery('danish',:"+cond.placeholder+")");
-
-            if (cond.column == 'adresse')
-                cond.setCondition("to_tsvector('danish',coalesce(gadenavn,' ')||' '||coalesce(postnr,' ')||' '||coalesce(postdistrikt,' ')||' '||coalesce(hus_nr::varchar,' ')||' '||coalesce(litra,' ')||' '||coalesce(etage,' ')) @@ websearch_to_tsquery('danish',:"+cond.placeholder+")");
+            console.log("streetname query, search <"+cond.getValue()+'>');
+            if (cond.column == 'street_name')
+                cond.setCondition("to_tsvector('danish',coalesce(street_name,' ')||' '||house_number||' '||coalesce(house_letter,' ')||' '||coalesce(floor::varchar,' ')||' '||coalesce(apartment,' ')) @@ websearch_to_tsquery('danish',:"+cond.placeholder+")");
         });
-
-        return(true);
-    }
-
-
-    @trigger(Trigger.PreQuery,"ordrer")
-    public async prequeryOrdrer(event:SQLTriggerEvent) : Promise<boolean>
-    {
-        let dates:DateUtils = new DateUtils();
-        let ctrl:Block = this.getBlock("ctrl");
-
-        let datefr:string = ">= "+dates.format(ctrl.getValue(0,"fra_dato"));
-        let dateto:string = "<= "+dates.format(ctrl.getValue(0,"til_dato"));
-
-        event.stmt.whand("udkomst_dato",datefr,Column.date);
-        event.stmt.whand("udkomst_dato",dateto,Column.date);
-
-        let type:string = ctrl.getValue(0,"type");
-
-        if (type != null && type.length > 0)
-            event.stmt.whand("subtype",type.toUpperCase());
-
-        return(true);
-    }
-
-
-    @connect
-    public async onConnect() : Promise<boolean>
-    {
-        this.kunder.enterquery();
-        return(true);
-    }
-
-
-    @show
-    public async init() : Promise<boolean>
-    {
-        let ctrl:Block = this.getBlock("ctrl");
-        await ctrl.createControlRecord();
-
-        ctrl.setFieldDefinition({name: "type", type: FieldType.dropdown});
-
-        let types:Set<string> = new Set<string>();
-
-        types.add("");
-        types.add("Salg");
-        types.add("Abon");
-        types.add("Post");
-        types.add("Depo");
-
-        ctrl.setPossibleValues("type",types,true);
-
-        let fra:Date = new Date();
-        let til:Date = new Date();
-        fra.setDate(fra.getDate()-7);
-
-        ctrl.setValue(0,"fra_dato",fra);
-        ctrl.setValue(0,"til_dato",til);
 
         return(true);
     }
